606f3956bd972641cfbd133df23f882ec83659bc
Éric  António <ericantoniodev@gmail.com>
2023-08-11 18:19:43 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    screens:{
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1140px'
+    },
+    extend: {
+
+      colors:{
+        brighRed:'hsl(12 , 88%, 59%)',
+        brighRedLight:'hsl(12,88%, 69%)',
+        brighRedSupLight:'hsl(12,88%,95%)',
+
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlues:'hsl(227, 12%, 61%)',
+
+        verDarkBlue:'hsl(223, 13%, 13%)',
+        veryPlaedRed:'hsl(13%, 100%, 96%)',
+        verLightGray:'hsl(0, 0%, 98%)',
+      }
+    },
   },
   plugins: [],
-}
-
+};
